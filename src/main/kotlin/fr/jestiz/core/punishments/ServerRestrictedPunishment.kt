03a671c274a708589af24b7493c06ca872c8aedd
Jules <jules.prn06@gmail.com>
2023-03-21 13:08:53 +0100

    Syncing to main thread when punishment kicks player

--- src/main/kotlin/fr/jestiz/core/punishments/ServerRestrictedPunishment.kt
+++ src/main/kotlin/fr/jestiz/core/punishments/ServerRestrictedPunishment.kt
@@ -1,11 +1,16 @@
 package fr.jestiz.core.punishments
 
+import fr.jestiz.core.Core
 import fr.jestiz.core.players.ServerPlayer
+import org.bukkit.Bukkit
 
 interface ServerRestrictedPunishment {
 
     fun kick(serverPlayer: ServerPlayer, msg: String) {
-        serverPlayer.player.kickPlayer(msg)
+        if (!Bukkit.isPrimaryThread())
+            Bukkit.getScheduler().runTask(Core.instance) { serverPlayer.player.kickPlayer(msg) }
+        else
+            serverPlayer.player.kickPlayer(msg)
     }
 
 }
